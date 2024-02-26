54800f3ae7ba615ad008edf07924f0e725129c02
hoidanit <hoangthinh9611@gmail.com>
2024-02-26 19:59:44 +0700

    3

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,21 +1,31 @@
 
 import db from '../models/index';
-let getHomePage = async(req, res) => {
+import CRUDService from "../service/CRUDService";
+let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
-    
+
         return res.render('homepage.ejs', {
             data: JSON.stringify(data)
         });
 
-    }catch(e) {
+    } catch (e) {
         console.log(e)
     }
-   
+
 }
 
 let getAboutPage = (req, res) => {
     return res.render('test/about.ejs');
+}
+
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs')
+}
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message)
+    return res.send('post CRUD');
 }
 
 // object: {
@@ -24,5 +34,7 @@ let getAboutPage = (req, res) => {
 // }
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage: getAboutPage
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
 }
